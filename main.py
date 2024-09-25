04fda9ca9c3c5bb0a35d69cf57c48f4a95941a6f
luojiahai <ljiahai@hotmail.com>
2024-09-25 19:30:11 +1000

    Update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import mocli
 
 if __name__ == '__main__':
-    mocli.hello()
+    mocli.run()
--- mocli/__init__.py
+++ mocli/__init__.py
@@ -1,2 +1,18 @@
-def hello():
-    print("Hello, World!")
+import cmd
+
+class Hello(cmd.Cmd):
+
+    prompt = "(mocli) > "
+
+    def do_hello(self, arg):
+        print("hello")
+    
+    def do_exit(self, arg):
+        print("bye")
+        return True
+    
+    def preloop(self):
+        return self.do_help('')
+
+def run():
+    Hello().cmdloop()
