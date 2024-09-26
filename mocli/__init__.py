f65e41ba27bf77e41dbe78b230f9175c86746378
luojiahai <ljiahai@hotmail.com>
2024-09-26 11:10:00 +1000

    Update

--- mocli/__init__.py
+++ mocli/__init__.py
@@ -1,18 +1,60 @@
+from __future__ import annotations
 import cmd
+from typing import Callable, List, Self
 
-class Hello(cmd.Cmd):
 
-    prompt = "(mocli) > "
+class Command:
 
-    def do_hello(self, arg):
-        print("hello")
-    
+    def __init__(self, name: str, fn: Callable) -> None:
+        self.name = name
+        self.fn = fn
+
+
+class CommandLineInterface(cmd.Cmd):
+
+    def __init__(self, builder: Builder) -> None:
+        super().__init__()
+        self.prompt = builder._prompt
+        for command in builder._commands:
+            setattr(self, f'do_{command.name}', command.fn)
+
+    @staticmethod
+    def builder() -> Builder:
+        return CommandLineInterface.Builder()
+
+    def get_names(self):
+        # This returns a list of all methods of the class
+        # including the dynamically added
+        return dir(self)
+
     def do_exit(self, arg):
         print("bye")
         return True
-    
+
     def preloop(self):
         return self.do_help('')
 
+    class Builder:
+
+        def __init__(self) -> None:
+            self._prompt = "(mocli) "
+            self._commands: List[Command] = []
+
+        def prompt(self, prompt: str) -> Self:
+            self._prompt = prompt
+            return self
+
+        def command(self, name: str, fn: Callable) -> Self:
+            self._commands.append(Command(name, fn))
+            return self
+
+        def build(self) -> CommandLineInterface:
+            return CommandLineInterface(self)
+
+
 def run():
-    Hello().cmdloop()
+    cli = CommandLineInterface.builder() \
+        .prompt("(my-cli) ") \
+        .command('hello', lambda arg: print("Hello, world!")) \
+        .build()
+    cli.cmdloop()
