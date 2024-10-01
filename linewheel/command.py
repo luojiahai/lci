5c34a628706dda8d380ea8acf6edbdad7892fa15
luojiahai <ljiahai@hotmail.com>
2024-10-01 19:05:19 +1000

    Update

--- linewheel/command.py
+++ linewheel/command.py
@@ -1,25 +1,25 @@
 import abc
 import subprocess
-from typing import Callable
+from typing import Callable, List
 
 
-def command_function(executable) -> Callable[[str], bool]:
-    def wrapper(arg) -> bool:
-        executable(arg)
+def command_function(executable) -> Callable[[List[str]], bool]:
+    def wrapper(args) -> bool:
+        executable(args)
         return False
     return wrapper
 
 
 class Command(abc.ABC):
 
-    def __init__(self, name: str, fn: Callable) -> None:
+    def __init__(self, name: str, fn: Callable[[List[str]], bool]) -> None:
         self.name = name
         self.fn = fn
 
 
 class Function(Command):
 
-    def __init__(self, name: str, fn: Callable) -> None:
+    def __init__(self, name: str, fn: Callable[[List[str]], None]) -> None:
         super().__init__(name, command_function(fn))
 
 
--- main.py
+++ main.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='lw')
     subparsers = parser.add_subparsers(dest='command')
     hello_parser = subparsers.add_parser(name='hello', help='this is a help for hello')
-    hello_parser.add_argument('arg', nargs='+', type=str)
+    hello_parser.add_argument('args', nargs='*', help='this is a help for args')
     ls_parser = subparsers.add_parser(name='ls', help='this is a help for ls')
 
     with lw.cli() as cli:
         cli.command(lw.Function(
             name='hello',
-            fn=lambda arg: print("Hello, World!" + arg),
+            fn=lambda args: print("Hello, World!", args),
         ))
         cli.command(lw.Subprocess(
             name='ls',
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     try:
         args = parser.parse_args()
         command = args.__getattribute__('command')
-        cli.commands[command].fn(args.__getattribute__('arg'))
+        cli.commands[command].fn(args.__getattribute__('args'))
     except Exception as e:
         print(e)
         parser.print_help()
